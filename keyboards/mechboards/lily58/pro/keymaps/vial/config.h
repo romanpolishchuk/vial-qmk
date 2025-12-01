aaa373bb1cbab34e979ee7a29df3a1812c47d478
Roman Polishchuk <roman.polishchuk@student.griffith.ie>
2025-12-01 23:23:43 +0000

    Add vial keymap

---
+++ keyboards/mechboards/lily58/pro/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x84, 0x01, 0x81, 0x34, 0x22, 0xEC, 0x27, 0xE5}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
